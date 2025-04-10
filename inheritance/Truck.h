a2426617197ef222a5ceb90d6c23d05337450b5d
buchert70427 <buchert70427@B124-19>
2025-04-10 09:47:14 -0400

    Built Truck.h

--- inheritance/Truck.h
+++ inheritance/Truck.h
@@ -1,4 +1,22 @@
 #pragma once
-class Truck
-{
+
+#ifndef TRUCK_H
+#define TRUCK_H
+
+#include "Vehicle.h"
+
+class Truck:public Vehicle{
+private:
+	int towingCapacity;
+
+public:
+	Truck();
+	Truck(string manufacturer, int year, int towingCapacity);
+
+	int getTowingCapacity() const;
+	void setTowingCapacity(int capacity);
+
+	void displayInfo() const override;   //this function is intended to override a virtual function with the same signature in a base class
 };
+
+#endif
