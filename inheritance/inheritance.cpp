ccf73e43c6e19cf1840e7e6ed7804da9acf13452
buchert70427 <buchert70427@B124-19>
2025-04-10 10:07:13 -0400

    Built main code

---
+++ inheritance/inheritance.cpp
@@ -0,0 +1,60 @@
+/**************
+*Cheyenne Buchert
+*CIS 1202
+*Inheritance
+*4/10/2025
+***************/
+
+#include <iostream>
+#include "Vehicle.h"
+#include "Car.h"
+#include "Truck.h"
+
+using namespace std;
+
+int main() {
+	string manufacturer;
+	int year, doors, towing;
+
+	//Vehicle
+	cout << "Vehicle Info/nEnter manufacturer: ";
+	getline(cin, manufacturer);
+	cout << "Enter year built: ";
+	cin >> year;
+	cin.ignore();  //flush newline
+
+	Vehicle myVehicle(manufacturer, year);
+	cout << "\nVehicle Information:\n";
+	myVehicle.displayInfo();
+
+
+	//Car
+	cout << "\nCar Info\nEnter manufacturer: ";
+	getline(cin, manufacturer);
+	cout << "Enter year built: ";
+	cin >> year;
+	cout << "Enter number of doors: ";
+	cin >> doors;
+	cin.ignore();
+
+	Car myCar(manufacturer, year, doors);
+	cout << "\nCar Information:\n";
+	myCar.displayInfo();
+
+	//Truck
+	cout << "\nTruck Info\nEnter manufacturer: ";
+	getline(cin, manufacturer);
+	cout << "Enter year built: ";
+	cin >> year;
+	cout << "Enter towing capacity: ";
+	cin >> towing;
+
+	Truck myTruck(manufacturer, year, towing);
+	cout << "\nTruck Information:\n";
+	myTruck.displayInfo();
+
+	cout << "Press enter to exit . . .";
+	cin.ignore();
+	cin.get();
+	return 0;
+}
