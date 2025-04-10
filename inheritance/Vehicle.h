254482965a9287e1d564fa147662c120e86e96fa
buchert70427 <buchert70427@B124-19>
2025-04-10 09:20:02 -0400

    Built Vehichle.h

--- inheritance/Vehicle.h
+++ inheritance/Vehicle.h
@@ -1,4 +1,26 @@
 #pragma once
+
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+#include <string>
+using namespace std;
+
+
 class Vehicle
 {
+private:
+	string manufacturer;
+	int yearBuilt;
+
+public:
+	Vehicle();
+	Vehicle(string manufacturer, int year);
+
+	string getManufacturer() const;
+	int getYearBuilt(int year);
+
+	virtual void displayInfo() const;
 };
+
+#endif
