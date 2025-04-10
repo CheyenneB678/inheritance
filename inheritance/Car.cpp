ea1be15334c8cd7d738466f21f3609083bb17c30
buchert70427 <buchert70427@B124-19>
2025-04-10 09:42:42 -0400

    Built Car.cpp

--- inheritance/Car.cpp
+++ inheritance/Car.cpp
@@ -1,1 +1,21 @@
 #include "Car.h"
+#include <iostream>
+
+Car::Car():Vehicle(), numberOfDoors(0){}  //initializer list
+
+Car::Car(string manufacturer, int year, int doors){
+	this->numberOfDoors = doors;
+}
+
+int Car::getNumberOfDoors() const{
+	return numberOfDoors;
+}
+
+void Car::setNumberOfDoors(int doors){
+	numberOfDoors = doors;
+}
+
+void Car::displayInfo() const{
+	Vehicle::displayInfo();
+	cout << "Number Of Doors: " << numberOfDoors << endl;
+}
